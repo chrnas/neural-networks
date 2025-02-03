04eb9ba3d0dcb3f14641558afdffc5d92695267b
Christoffer Näs <christoffernas2001@gmail.com>
2025-02-03 16:28:05 +0100

    init

---
+++ A1_Supervised/evalFunctions.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def calcAccuracy(LPred, LTrue):
+    """Calculates prediction accuracy from data labels.
+
+    Args:
+        LPred (array): Predicted data labels.
+        LTrue (array): Ground truth data labels.
+
+    Retruns:
+        acc (float): Prediction accuracy.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    acc = np.mean(LPred == LTrue)
+    # ============================================
+    return acc
+
+
+def calcConfusionMatrix(LPred, LTrue):
+    """Calculates a confusion matrix from data labels.
+
+    Args:
+        LPred (array): Predicted data labels.
+        LTrue (array): Ground truth data labels.
+
+    Returns:
+        cM (array): Confusion matrix, with predicted labels in the rows
+            and actual labels in the columns.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    classes = np.unique(LTrue)
+    cM = np.zeros((len(classes), len(classes)))
+
+    for true, pred in zip(LTrue, LPred):
+        cM[true, pred] += 1
+    # ============================================
+
+    return cM
+
+
+def calcAccuracyCM(cM):
+    """Calculates prediction accuracy from a confusion matrix.
+
+    Args:
+        cM (array): Confusion matrix, with predicted labels in the rows
+            and actual labels in the columns.
+
+    Returns:
+        acc (float): Prediction accuracy.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    acc = np.sum(np.diagonal(cM))/np.sum(cM)
+    # ============================================
+    
+    return acc
